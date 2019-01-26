ed5766fec01add4c84caa71b22615dce14a96f2a
stevegordon <tdtsgordon@gmail.com>
2019-01-25 23:54:51 -0800

    add box and box methods

--- app.js
+++ app.js
@@ -599,4 +599,40 @@ function reliableMultiply(a,b) {
 }
 console.log(reliableMultiply(8,8));
 
-// following ....
+// Lockbox
+const box = {
+  locked: true,
+  unlock() {this.locked = false},
+  lock() {this.locked = true},
+  _content: [],
+  get content() {
+    if (this.locked) throw new Error("Locked!");
+    return this._content;
+  }
+};
+
+function withBoxUnlocked(body) {
+  let open = false;
+  if (!box.locked) {open = true;}
+  box.unlock();
+  try {
+    body();
+  }
+  finally {
+    if (!open) {box.lock();}
+  }
+}
+
+withBoxUnlocked(function() {
+  box.content.push("gold piece");
+});
+
+try {
+  withBoxUnlocked(function() {
+    throw new Error("Pirates on the horizon! Abort!");
+  });
+}
+catch (e) {
+  console.log("Error raised:", e);
+}
+console.log(box.locked);
